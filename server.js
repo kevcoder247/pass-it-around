0bfcbc85e4862441ef5957ecad20b82a99d686ca
)- <a-@-s-MacBook-Pro.local>
2022-05-30 19:27:18 -0400

    links and logic added

--- server.js
+++ server.js
@@ -10,9 +10,16 @@ app.get('/', (req, res) => {
     <a href="/98">take one down and pass it around =></a> `)
 })
 
-//Route that takes Parameter and checks if the number of beers left is greater than 0
+//Create a route that takes Parameter and checks if the number of beers left is greater than 0
+// if beer left greater than 0 link keeps decreasing amount of beers left else until 0 is reached
 app.get('/:beersLeft', (req, res) => {
-   res.send('hello')
+   if(req.params.beersLeft <= 0){
+       res.send(`${req.params.beersLeft} Beers left on the wall <br/>
+         <a href="/"> now put them back and start over</a>`)
+   }else{
+       res.send(`${req.params.beersLeft} Beers left on the wall
+        <a href=${req.params.beersLeft - 1}> take one down and pass it around</a>                                                    `)
+   }
 })
 
 
